dc7db4d771d020f984ec9b8bc10e4f050967b13a
Samuel Odebaran <125310099+OdebaranSamuel@users.noreply.github.com>
2023-07-16 17:03:07 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -20,27 +20,3 @@ const enviaDados = (evento) => {
 input[1].addEventListener("input", validaDados);
 
 formulario.addEventListener("submit", enviaDados);
-// const validaDados = (element) => {
-//     if (element.value.length >= 3) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   };
-
-//   const input = Array.from(document.querySelectorAll(".input-login"));
-//   const botao = document.querySelector(".submit-login");
-
-//   input.forEach((item) =>
-//     item.addEventListener("input", () => {
-// Verifica se ambos os item foram validados. O map retorna um array com o resultado da validação. O includes verifica se alguma validação foi false.
-//   const invalido = input.map((item) => validaDados(item)).includes(false);
-//   if (!invalido) {
-//     botao.removeAttribute("disabled");
-//   }
-// })
-//   );
-
-// [sa,od]
-//validaDados retorna false. includes false? sim (true)
-//se não for invalido, ou seja, invalido recebeu true. Se nao for verdadeiro removo
